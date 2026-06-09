62e72ef4924830b7760dfb3670adcfbb0697cc08
slaterdaniel <dslater2028@mka.org>
2026-06-09 11:13:23 -0400

    WaitingForRep page stylized

--- SmartSole/testing/WaitingForRep.swift
+++ SmartSole/testing/WaitingForRep.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 
-struct DataDisplayPage: View {
+struct WaitingForRep: View {
     @Environment(\.dismiss) var dismiss
 
     var body: some View {
         NavigationStack {
             Spacer()
-            
-            Text("Waiting For Rep to Start")
-            
+                .frame(height: 275)
+            Text("Waiting for Rep to Start")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.blue.gradient)
             Spacer()
-            
+                .frame(height: 50)
+            VStack (spacing: 20) {
+                HStack{
+                    Text("Leading Foot")
+                    Text("Down")
+                        .underline()
+                }
+                HStack {
+                    Text("Trailing Foot on")
+                    Text("Toe")
+                        .underline()
+                }
+                Spacer()
+                    .frame(height: 20)
+                Text("Step")
+                Image(systemName: "arrow.down")
+                Text("Hang")
+                Image(systemName: "arrow.down")
+                Text("Takeoff")
+
+            }
+            .fontWeight(.ultraLight)
+            .frame(maxWidth: 300)
+            Spacer()
             Button("Cancel Analysis") {
                 dismiss()
             }
@@ -29,9 +54,8 @@ struct DataDisplayPage: View {
             .overlay(
                 RoundedRectangle(cornerRadius: 5)
                     .stroke()
-                    .opacity(0.5)
             )
-            .foregroundStyle(.red.opacity(0.5).gradient)
+            .foregroundStyle(.red.opacity(0.75).gradient)
             .frame(maxWidth: .infinity)
         }
         .navigationBarBackButtonHidden()
