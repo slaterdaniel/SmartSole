da51e8c8b8dc6afbe16e5dae2216f2cd9d5aff3d
slaterdaniel <dslater2028@mka.org>
2026-06-27 14:40:54 -0400

    add pattern recognition to force sensor testing

--- Insole Arduino Code/force_sensor/force_sensor.ino
+++ Insole Arduino Code/force_sensor/force_sensor.ino
@@ -4,9 +4,7 @@
 
 #include "Constants.h"
 
-
 MPR121 mpr121;
-
 void setup()
 {
   Serial.begin(constants::baud);
@@ -34,6 +32,65 @@ void setup()
     constants::sample_period);
 
   mpr121.startAllChannels(constants::proximity_mode);
+
+  // int8_t baselines[constants::physical_channel_count];
+  // for (uint8_t i=0; i<constants::physical_channel_count; i++) {
+  //   if (!mpr121.communicating(constants::device_address)) {
+  //     Serial << "mpr121 device not commmunicating!\n\n";
+  //     while(1);
+  //   }
+  //   baselines[i] = mpr121.getChannelFilteredData(i);
+  // }
+
+  // find when the rep starts
+  float pattern_start;    
+  bool pattern_active = false;
+
+  while (1) {
+    // BLE.poll();
+    if (1) {
+        // listen for force distribution
+        // BREAK when start sequence is found:
+        // - front foot fully down (2sec)
+        // - back foot on toe (2sec)
+
+        // delay(constants::loop_delay);
+
+        // String forces;
+        int8_t forces_list[constants::physical_channel_count];
+        for (uint8_t i=0; i < constants::physical_channel_count; i++) {
+            int8_t difference = mpr121.getChannelBaselineData(i) - mpr121.getChannelFilteredData(i);
+            // forces += String(difference) + ',';
+            forces_list[i] = difference;
+            Serial.print(forces_list[i]);
+            Serial.print("  ");
+        }
+        Serial.println();      
+        if ( // if pattern is recognized
+            forces_list[0]  < -constants::touch_threshold && // heel off the ground
+            forces_list[1]  < -constants::touch_threshold &&
+            forces_list[2]  < -constants::touch_threshold &&
+            forces_list[3]  < -constants::touch_threshold && 
+
+            forces_list[8]  >  constants::touch_threshold && // toe on the ground
+            forces_list[9]  >  constants::touch_threshold &&
+            forces_list[10] >  constants::touch_threshold &&
+            forces_list[11] >  constants::touch_threshold
+        ) {
+            if (!pattern_active) { // if pattern just started -> mark starting time
+                pattern_active = true;
+                pattern_start = millis();
+            }
+            else if (millis() - pattern_start >= 2000) { // 2000 = 2 seconds
+                break; // if pattern found and time > 2sec -> start loop()
+                // Serial.println("BREAKING");
+            }
+        }
+        else { // if pattern is not recognized -> reset
+            pattern_active = false;
+        }
+    }
+  }  
 }
 
 void loop()
@@ -41,8 +98,7 @@ void loop()
   delay(constants::loop_delay);
   if (!mpr121.communicating(constants::device_address))
   {
-    // Serial << "mpr121 device not commmunicating!\n\n";
-    Serial.println("12,34,56,78,910");
+    Serial << "mpr121 device not commmunicating!\n\n";
     return;
   }
 
